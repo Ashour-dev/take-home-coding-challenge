528f54d8f30ab670e6ed10012bdd2b895437b343
Ashour100 <interinstitute5@gmail.com>
2022-10-03 15:56:13 +0200

    Data put in a better way

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,14 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cart</title>
 </head>
 <body>
     <h1>Cart</h1>
     @if (count($cart)!=0)
-    {{dd($cart)}}
+    {{-- {{dd($cart)}} --}}
+    <div class="container-fluid">
+        <div class="row w-100 justify-content-center">
+            <div class="col-11 mx-auto">
+                <table class="table w-100 table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">type</th>
+                            <th scope="col">price</th>
+                            <th scope="col">Shipped from</th>
+                            <th scope="col">Weight</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($cart as $product)
+                            <tr class="products-index-table">
+                                <td>{{$product['name']}}</td>
+                                <td>{{$product['Item_type']}}</td>
+                                <td>{{$product['Item_price']}}</td>
+                                <td>{{$product['Country']}}</td>
+                                <td>{{$product['Weight']}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     @else
-    <h3>There is nothing in here</h3>
+    <h3>Your Cart is Empty</h3>
     @endif
 </body>
 </html>
