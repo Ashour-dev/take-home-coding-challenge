0245f8183205e9440362f1432e1f984fd1bd498e
Ashour100 <interinstitute5@gmail.com>
2022-10-03 15:21:30 +0200

    Cart table created

---
+++ database/migrations/2022_10_03_131647_create_carts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('Item_type');
+            $table->char('Country',3);
+            $table->float('Item_price',3,2);
+            $table->float('Weight',2,1);
+            $table->integer('Rate');
+            $table->float('Shipping',3,2);
+            $table->float('VAT',4,4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
