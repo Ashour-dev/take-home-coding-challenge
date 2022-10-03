629fc9c028dde91b09a1609661db97dc72c1344b
Ashour100 <interinstitute5@gmail.com>
2022-10-03 18:06:19 +0200

    New values validation and assignment + shipping calculation

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -36,8 +36,44 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'name' => ['required', 'string', 'min:5','max:255'],
+            'Item_type' => ['required', 'string', 'min:3','max:255'],
+            'link' => ['required', 'string','min:10','max:65000'],
+            'price' => ['required', 'numeric','min:1'],
+            'quantity' => ['required', 'numeric','min:1','max:5'],
+        ]);
+        $data = $request->all();
+
+        $newItem = new Cart();
+        $newItem->name = $data["name"];
+        $newItem->Item_type = $data["Item_type"];
+        $newItem->Country = $data["Country"];
+        $newItem->quantity = $data["quantity"];
+        $newItem->Item_price = $data["Item_price"]*$data["quantity"];
+        $newItem->Weight = $data["Weight"];
+        $newItem->link = $data["link"];
+        if($data["Country"]=='UK'||$data["Country"]=='CN')
+            $rate=2;
+        else
+            $rate=3;
+        
+        $newItem->Rate =$rate;
+        $newItem->Shipping = ($data["Weight"]*10)*$rate;
+        // $newItem->VAT = 'To be calculated';
+        $newItem->save();
+        }
+        // switch($data["Country"]){
+        //     case 'US':
+        //         $rate=2;
+        //         break;
+        //     case 'UK':
+        //         $rate=3;
+        //         break;
+        //     case 'CN':
+        //         $rate=2;
+        //         break;
+        // }
 
     /**
      * Display the specified resource.
